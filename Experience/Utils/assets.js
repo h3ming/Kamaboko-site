fe3f93a0b2473b218027468b1e6f4b9c06f6fce6
h3ming <62722344+h3ming@users.noreply.github.com>
2023-03-23 13:41:09 -0500

    video loader issue

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/roomFront.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture", 
+        path: "/textures/how to make thin hamster.mp4",
+    }
+]
